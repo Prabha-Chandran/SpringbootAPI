f09cee43cb9e79b96f6cc892f46ccd94df2cd905
“Prabha-Chandran” <prabha.c@comakeit.com>
2023-07-05 02:51:37 -0700

    springboot project setup

---
+++ restapi-crudapp/src/main/java/com/blueberry/restapicrudapp/RestapiCrudappApplication.java
@@ -0,0 +1,13 @@
+package com.blueberry.restapicrudapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RestapiCrudappApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RestapiCrudappApplication.class, args);
+	}
+
+}
